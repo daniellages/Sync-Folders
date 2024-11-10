f87b419c093476df8895e9ae6c1a2fdbb0c2278d
Daniel Lages <daniel.lages.2000@gmail.com>
2024-11-10 11:36:37 +0000

    Log actions in file
    Actions are now logged in file and console

--- main.py
+++ main.py
@@ -8,9 +8,6 @@
 #   - Synchronization performed periodically
 #   - Log actions
 #   - Ask user inputs
-#
-# TODO:
-#   Log actions in a different file
 
 import os
 import shutil
@@ -19,7 +16,7 @@ import time
 from pathlib import Path
 
 # Check if folders exist
-def check_folders(source, replica, log):
+def check_paths(source, replica, log):
     if not os.path.exists(source):
         print(f"Source folder '{source}' does not exist.")
         return False
@@ -27,9 +24,20 @@ def check_folders(source, replica, log):
         print(f"Replica folder '{replica}' does not exist.")
         return False
     if not os.path.exists(log):
-        print(f"Log folder '{log}' does not exist.")
+        print(f"Log file '{log}' does not exist.")
+        return False
+    if not os.path.splitext(log)[1].lower() == ".txt":
+        print(f"Log file '{log}' is not a text file.")
         return False
     return True
+
+# Logging messages to both file and console
+def log_action(log_file, msg):
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    full_msg = f"'{timestamp}' {msg}"
+    print(full_msg)     # log on console
+    with open(log_file, 'a') as log:
+        log.write(full_msg + '\n')  # log in file
 
 # Calculade MD5 hash
 def md5_hash(path):
@@ -57,7 +65,7 @@ def md5_hash(path):
     return hash.hexdigest()
 
 # One-way synchronization
-def sync_folders(source, replica):
+def sync_folders(source, replica, log):
 
     # Get all items in source folder
     items_src = []
@@ -78,37 +86,37 @@ def sync_folders(source, replica):
             # Check for modifications
             else:
                 if md5_hash(item_src) != md5_hash(item_rep):
-                    print(f"Updating folder '{item_rep}'")
+                    log_action(log, f"Updating folder '{item_rep}'")
                     shutil.rmtree(item_rep)                 # remove outdated folder
                     shutil.copytree(item_src, item_rep)     # copy folder
         # Handle files
         else:
             if not os.path.exists(item_rep):
-                print(f"Copying '{item_src}' to '{item_rep}'")  # log on console
+                log_action(log, f"Copying '{item_src}' to '{item_rep}'")  # log on console
                 shutil.copy2(item_src, item_rep)
             else:
                 # Check for modifications
                 if md5_hash(item_src) != md5_hash(item_rep):
-                    print(f"Updating file '{item_rep}'")
+                    log_action(log, f"Updating file '{item_rep}'")
                     shutil.copy2(item_src, item_rep)
 
     # Deletes items in replica that are not in source
-    delete_extra_items(source, replica)
+    delete_extra_items(source, replica, log)
 
-def delete_extra_items(source, replica):
+def delete_extra_items(source, replica, log):
     for dirpath, dirnames, filenames in os.walk(replica):
         for file in filenames:
             file_rep = os.path.join(dirpath, file)
             file_src = os.path.join(source, os.path.relpath(file_rep, replica)) # corresponding path in the source folder
             if not os.path.exists(file_src):
-                print(f"Deleting file '{file_rep}'")
+                log_action(log, f"Deleting file '{file_rep}'")
                 os.remove(file_rep)
 
         for folder in dirnames:
             folder_rep = os.path.join(dirpath, folder)
             folder_src = os.path.join(source, os.path.relpath(folder_rep, replica))
             if not os.path.exists(folder_src):
-                print(f"Deleting folder '{folder_rep}'")
+                log_action(log, f"Deleting folder '{folder_rep}'")
                 os.rmdir(folder_rep)
 
 def main():
@@ -117,11 +125,11 @@ def main():
     source_path = input("Enter source folder path:")
     replica_path = input("Enter replica folder path:")
     interval = int(input("Enter synchronization interval (seconds):"))
-    log_path = input("Enter log folder path:")
+    log_path = input("Enter log file path:")
     
-    if check_folders(source_path, replica_path, log_path):
+    if check_paths(source_path, replica_path, log_path):
         while True:
-            sync_folders(source_path, replica_path)
+            sync_folders(source_path, replica_path, log_path)
             print(f"Folders synchronized! Waiting '{interval}' seconds for next iteration...")
             time.sleep(interval)    # Synchronized periodically
 
